bfb5566fcbf89b0f6222967f30d0d659938186a4
Max-Tkachenko <unluuucky.officail@gmail.com>
2017-12-06 20:57:46 +0200

    First Test

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -12,7 +12,7 @@ import liquibase.resource.FileSystemResourceAccessor;
 import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LiquibaseException {
         Connect.connect();
         try {
             testMigrations();
@@ -24,45 +24,93 @@ public class Main {
     }
 
     public static void testMigrations() throws LiquibaseException {
+        create_tables();
+        set_data();
+        set_row();
+        update_row();
+        query_join();
+        rename_column();
+        new_column();
+        new_table();
+        delete_row();
+        delete_all_rows();
+        drop_tables();
+    }
 
+    public static void create_tables() throws LiquibaseException {
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
-
         File change_create_tables = new File(ClassLoader.getSystemResource("liquibase/1.0-create-tables.xml").getFile());
-        File change_set_data = new File(ClassLoader.getSystemResource("liquibase/3.0-set-data.xml").getFile());
-        File change_set_row = new File(ClassLoader.getSystemResource("liquibase/3.1-set-row.xml").getFile());
-        File change_update_row = new File(ClassLoader.getSystemResource("liquibase/3.2-update-row.xml").getFile());
-        File change_join = new File(ClassLoader.getSystemResource("liquibase/3.3-query-join.xml").getFile());
-        File change_rename = new File(ClassLoader.getSystemResource("liquibase/5.0-rename-column.xml").getFile());
-        File change_new_column = new File(ClassLoader.getSystemResource("liquibase/5.1-new-column.xml").getFile());
-        File change_new_table = new File(ClassLoader.getSystemResource("liquibase/5.2-new-table.xml").getFile());
-        File change_delete_row = new File(ClassLoader.getSystemResource("liquibase/4.1-delete-row.xml").getFile());
-        File change_delete_rows = new File(ClassLoader.getSystemResource("liquibase/4.0-delete-all-rows.xml").getFile());
-        File change_drop_tables = new File(ClassLoader.getSystemResource("liquibase/2.0-drop-tables.xml").getFile());
-
         Liquibase create_tables = new liquibase.Liquibase(change_create_tables.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase set_data = new liquibase.Liquibase(change_set_data.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase set_row = new liquibase.Liquibase(change_set_row.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase update_row = new liquibase.Liquibase(change_update_row.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase query_join = new liquibase.Liquibase(change_join.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase rename_column = new liquibase.Liquibase(change_rename.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase new_column = new liquibase.Liquibase(change_new_column.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase new_table = new liquibase.Liquibase(change_new_table.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase delete_row = new liquibase.Liquibase(change_delete_row.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase delete_all_rows = new liquibase.Liquibase(change_delete_rows.toString(), new FileSystemResourceAccessor(), database);
-        Liquibase drop_tables = new liquibase.Liquibase(change_drop_tables.toString(), new FileSystemResourceAccessor(), database);
-
         create_tables.update(new Contexts(), new LabelExpression());
-        set_data.update(new Contexts(), new LabelExpression());
-        set_row.update(new Contexts(), new LabelExpression());
-        set_row.update(new Contexts(), new LabelExpression());
-        update_row.update(new Contexts(), new LabelExpression());
-        query_join.update(new Contexts(), new LabelExpression());
-        rename_column.update(new Contexts(), new LabelExpression());
-        new_column.update(new Contexts(), new LabelExpression());
-        new_table.update(new Contexts(), new LabelExpression());
-        delete_row.update(new Contexts(), new LabelExpression());
-        delete_all_rows.update(new Contexts(), new LabelExpression());
-        drop_tables.update(new Contexts(), new LabelExpression());
+    }
 
+    public static void set_data() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_set_data = new File(ClassLoader.getSystemResource("liquibase/3.0-set-data.xml").getFile());
+        Liquibase set_data = new liquibase.Liquibase(change_set_data.toString(), new FileSystemResourceAccessor(), database);
+        set_data.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void set_row() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_set_row = new File(ClassLoader.getSystemResource("liquibase/3.1-set-row.xml").getFile());
+        Liquibase set_row = new liquibase.Liquibase(change_set_row.toString(), new FileSystemResourceAccessor(), database);
+        set_row.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void update_row() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_update_row = new File(ClassLoader.getSystemResource("liquibase/3.2-update-row.xml").getFile());
+        Liquibase update_row = new liquibase.Liquibase(change_update_row.toString(), new FileSystemResourceAccessor(), database);
+        update_row.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void query_join() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_join = new File(ClassLoader.getSystemResource("liquibase/3.3-query-join.xml").getFile());
+        Liquibase query_join = new liquibase.Liquibase(change_join.toString(), new FileSystemResourceAccessor(), database);
+        query_join.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void rename_column() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_rename = new File(ClassLoader.getSystemResource("liquibase/5.0-rename-column.xml").getFile());
+        Liquibase rename_column = new liquibase.Liquibase(change_rename.toString(), new FileSystemResourceAccessor(), database);
+        rename_column.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void new_column() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_new_column = new File(ClassLoader.getSystemResource("liquibase/5.1-new-column.xml").getFile());
+        Liquibase new_column = new liquibase.Liquibase(change_new_column.toString(), new FileSystemResourceAccessor(), database);
+        new_column.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void new_table() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_new_table = new File(ClassLoader.getSystemResource("liquibase/5.2-new-table.xml").getFile());
+        Liquibase new_table = new liquibase.Liquibase(change_new_table.toString(), new FileSystemResourceAccessor(), database);
+        new_table.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void delete_row() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_delete_row = new File(ClassLoader.getSystemResource("liquibase/4.1-delete-row.xml").getFile());
+        Liquibase delete_row = new liquibase.Liquibase(change_delete_row.toString(), new FileSystemResourceAccessor(), database);
+        delete_row.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void delete_all_rows() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_delete_rows = new File(ClassLoader.getSystemResource("liquibase/4.0-delete-all-rows.xml").getFile());
+        Liquibase delete_all_rows = new liquibase.Liquibase(change_delete_rows.toString(), new FileSystemResourceAccessor(), database);
+        delete_all_rows.update(new Contexts(), new LabelExpression());
+    }
+
+    public static void drop_tables() throws LiquibaseException{
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(Connect.connection));
+        File change_drop_tables = new File(ClassLoader.getSystemResource("liquibase/2.0-drop-tables.xml").getFile());
+        Liquibase drop_tables = new liquibase.Liquibase(change_drop_tables.toString(), new FileSystemResourceAccessor(), database);
+        drop_tables.update(new Contexts(), new LabelExpression());
     }
 }
---
+++ src/test/java/AllMigrationsTest.java
@@ -0,0 +1,91 @@
+import items.*;
+import liquibase.exception.LiquibaseException;
+import org.junit.*;
+import run.Connect;
+import run.GetDataFromDB;
+import run.Main;
+
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+public class AllMigrationsTest {
+
+    public static List<Chat> chats = new ArrayList<>();
+    public static List<Message> messages = new ArrayList<>();
+    public static List<News> news = new ArrayList<>();
+
+    @BeforeClass
+    public static void onlyOnce() {
+        Connect.connect();
+    }
+
+    @AfterClass
+    public static void afterTest() {
+        Connect.disconnect();
+    }
+
+    @Test
+    public void createTablesTest() throws SQLException, LiquibaseException {
+        Main.create_tables();
+        DatabaseMetaData md = Connect.connection.getMetaData ();
+        ResultSet rs = md.getTables ("messanger_db", "", "%", null);
+        while (rs.next ())
+            System.out.println (rs.getString (3)); // столбец 3 = имя таблицы
+        rs.close ();
+    }
+
+    @Test
+    public void setDataTest1() throws SQLException, LiquibaseException {
+        Main.set_data();
+        GetDataFromDB.getData();
+        Chat chat = new Chat(1, "KPI Chan", "2010-05-10");
+        assertEquals(true, chat.equals(GetDataFromDB.chats.get(0)));
+    }
+
+    @Test
+    public void setDataTest2() {
+        Message message = new Message(1, 1, 1, "2014-05-23 19:52:10.0", "Привет!");
+        assertEquals(true, message.equals(GetDataFromDB.messages.get(0)));
+    }
+
+    @Test
+    public void setDataTest3() {
+        News news = new News(2, 2, "2015-04-23 19:48:10.0", "Голосование за ГБ2");
+        assertEquals(true, news.equals(GetDataFromDB.news.get(1)));
+    }
+
+    @Test
+    public void setRowTest() {
+
+    }
+
+    @Test
+    public void updateRowTest() {
+
+    }
+
+    @Test
+    public void queryJoinTest() {
+
+    }
+
+    @Test
+    public void renameColumnTest() {
+
+    }
+
+    @Test
+    public void copyTableTest() {
+
+    }
+
+    @Test
+    public void dropTablseTest() {
+
+    }
+}
